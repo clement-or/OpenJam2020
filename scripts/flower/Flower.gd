ae754dc7aeb6aa6c46969c7c8c903b4f461d61f3
BroZ <ludovicwarin53@gmail.com>
2020-09-19 15:07:35 +0200

    animation_&_script

--- scripts/flower/Flower.gd
+++ scripts/flower/Flower.gd
@@ -1,19 +1,35 @@
 extends Node2D
+signal has_been_entered(flower_encountered)
 
 var id = "flower01"
-var flower_name = "" #would be equal something in data ling by ID
-var flower_nectar
-var stem_height = 800 #would be equal something in data ling by ID
+var flower_name = "" #Valeur ajoutée à l'avenir avec un tableau de valeur
+var nectar = { #Les valeurs du nectar seront récupérées en fonction de l'ID de la fleur dans un tableau de valeur
+	"name": "base_nectar",
+	"value": 1,
+	"color": Color("#a5da00")
+	}
+#var stem_height = 400 #Pas sûr de l'utilité, garder ça dans le coin de la tête
 var is_polinised = true
 var is_gleaned = false
 
-func _process(delta):
-	if(is_polinised && is_gleaned):
-		$RenewNectarTimer.start()
-
 func _on_RenewNectarTimer_timeout():
 	is_gleaned = false
+	$Head.modulate = Color(1,1,1,1)
 
-func get_gleaned():
+func has_been_gleaned():
 	is_gleaned = true
 	$Head.modulate = Color(1,1,1,0.5)
+	if(is_polinised):
+		$RenewNectarTimer.start()
+
+func has_been_polinised():
+	is_polinised = true
+	if(is_gleaned):
+		$RenewNectarTimer.start()
+
+func get_nectar():
+	return nectar
+
+func _on_Area_body_entered(body):
+	if(body.get_class() == "Player"):
+		emit_signal("has_been_entered", self)
