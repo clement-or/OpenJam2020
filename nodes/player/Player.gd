9a45c2e476ba3cadf65dacd25df561b6c03c0899
Clément <clement.orlandini@gmail.com>
2020-09-19 14:33:22 +0200

    Déplacement abeille

--- nodes/player/Player.gd
+++ nodes/player/Player.gd
@@ -8,8 +8,13 @@ export var v_max_speed = 100
 export var h_acceleration = 10
 export var h_max_speed = 150
 
-# The velocity of the bee
+# La valeur de vol 
+export var max_flight = 100
+var cur_flight = 100
+
+# La vélocité de l'abeille
 var velocity = Vector2.ZERO
+var acceleration = Vector2.ZERO
 
 
 func _ready():
@@ -25,12 +30,22 @@ func _movement():
 	var x_vel = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	var y_vel = int(Input.is_action_pressed("ui_up"))
 	
+	# Soustraire la valeur de vol
+	if Input.is_action_pressed("ui_up"):
+		cur_flight -= .1
+	
+	# Empêcher le joueur de voler si il n'a plus de vol disponible
+	if cur_flight <= 0:
+		y_vel = 0
+	
 	# Mouvement vertical
-	velocity.y -= y_vel * v_acceleration
+	acceleration.y = - y_vel * v_acceleration
+	velocity.y += acceleration.y
 	velocity.y = clamp(velocity.y, -v_max_speed, 1000)
 	
 	# Mouvement horizontal
-	velocity.x += x_vel * v_acceleration
+	acceleration.x = x_vel * h_acceleration
+	velocity.x += acceleration.x
 	if !x_vel:
 		velocity.x = lerp(velocity.x, 0, .8)
 	velocity.x = clamp(velocity.x, -h_max_speed, h_max_speed)
@@ -41,7 +56,15 @@ func _movement():
 	
 	# Appliquer le mouvement
 	velocity = move_and_slide(velocity)
+	
+	# Dessiner les choses définies dans la fonction _draw()
+	update()
 
 
 func _animation():
 	$Sprite.flip_h = velocity.x < 0
+
+func _draw():
+	return
+	draw_line(position, position+acceleration, Color.red)
+	draw_circle(position+acceleration, 2, Color.red)
