96dccdb0491a4c88450cc7f528f536ef4bbf6610
Celimage <celie.pautrel@gmail.com>
2020-09-19 16:58:58 +0200

    Parallax start

---
+++ nodes/player/camera/Pivot.gd
@@ -0,0 +1,12 @@
+extends Position2D
+
+onready var parent = get_parent()
+
+func _ready():
+	_update_pivot_angle()
+	
+#func _physics_process():
+#	_update_pivot_angle()
+
+func _update_pivot_angle():
+	pass
